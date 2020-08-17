4c99d8ec74b4d2d1d38358564a6d79a2999d740a
garcialuis <garcialuisdev@gmail.com>
2020-08-17 16:55:51 -0700

    + Added foreign key in FoodItem table to reference FoodVariety table

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -7,18 +7,35 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+var carbohydrates = []models.CarbLevel{
+	models.CarbLevel{
+		Description: "lowest carbohydrate",
+	},
+	models.CarbLevel{
+		Description: "moderate carbohydrate",
+	},
+	models.CarbLevel{
+		Description: "highest carbohydrate",
+	},
+}
+
 func Load(db *gorm.DB) {
 
-	err := db.Debug().DropTableIfExists(&models.FoodItem{}, &models.CarbLevel{}, &models.FoodGroup{}).Error
+	err := db.Debug().DropTableIfExists(&models.FoodItem{}, &models.CarbLevel{}, &models.FoodGroup{}, &models.FoodVariety{}).Error
 
 	if err != nil {
 		log.Fatalf("cannot drop table: %v", err)
 	}
 
-	err = db.Debug().AutoMigrate(&models.CarbLevel{}, &models.FoodGroup{}, &models.FoodItem{}).Error
+	err = db.Debug().AutoMigrate(&models.FoodVariety{}, &models.CarbLevel{}, &models.FoodGroup{}, &models.FoodItem{}).Error
 
 	if err != nil {
 		log.Fatalf("cannot migrate table(s): %v", err)
+	}
+
+	err = db.Debug().Model(&models.FoodItem{}).AddForeignKey("food_variety_id", "food_varieties(id)", "cascade", "cascade").Error
+	if err != nil {
+		log.Fatalf("attaching foreign key error: %v", err)
 	}
 
 	err = db.Debug().Model(&models.FoodItem{}).AddForeignKey("carb_level_id", "carb_levels(id)", "cascade", "cascade").Error
@@ -29,5 +46,12 @@ func Load(db *gorm.DB) {
 	err = db.Debug().Model(&models.FoodItem{}).AddForeignKey("food_group_id", "food_groups(id)", "cascade", "cascade").Error
 	if err != nil {
 		log.Fatalf("attaching foreign key error: %v", err)
+	}
+
+	for i := range carbohydrates {
+		err = db.Debug().Model(&models.CarbLevel{}).Create(&carbohydrates[i]).Error
+		if err != nil {
+			log.Fatalf("Could not seed table with Carbohydrate levels: %v", err)
+		}
 	}
 }
