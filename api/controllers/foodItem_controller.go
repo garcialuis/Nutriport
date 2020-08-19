86da23b5b638b6b23bf9083a81d6ea4f8739e4ee
garcialuis <garcialuisdev@gmail.com>
2020-08-18 17:47:00 -0700

    +Added two functions to foodItem_controller to save new item and to find an item w/a specified name

---
+++ api/controllers/foodItem_controller.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/garcialuis/Nutriport/api/models"
+	"github.com/garcialuis/Nutriport/api/responses"
+	"github.com/gorilla/mux"
+)
+
+func (server *Server) CreateFoodItem(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	}
+
+	foodItem := models.FoodItem{}
+	err = json.Unmarshal(body, &foodItem)
+	fmt.Println("FoodItem Received: ", foodItem)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+	}
+
+	foodItemCreated, err := foodItem.SaveFoodItem(server.DB)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, foodItemCreated.ID))
+	responses.JSON(w, http.StatusCreated, foodItemCreated)
+}
+
+func (server *Server) GetFoodItemByName(w http.ResponseWriter, r *http.Request) {
+
+	foodItem := models.FoodItem{}
+	var err error
+
+	vars := mux.Vars(r)
+	foodName := vars["foodName"]
+
+	if len(foodName) < 1 {
+		err = fmt.Errorf("Invalid string length")
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	itemFound, err := foodItem.SelectByName(server.DB, foodName)
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, itemFound)
+}
