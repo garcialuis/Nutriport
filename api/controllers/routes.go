63a483ea3ea3e9642eab935e6fed48afd7a7d2bd
garcialuis <garcialuisdev@gmail.com>
2020-08-18 17:48:17 -0700

    +Added routes to save and find a food item

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -5,4 +5,8 @@ import "github.com/garcialuis/Nutriport/api/middlewares"
 func (s *Server) InitializeRoutes() {
 	// Home
 	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
+
+	// FoodItems
+	s.Router.HandleFunc("/fooditem/{foodName}", middlewares.SetMiddlewareJSON(s.GetFoodItemByName)).Methods("GET")
+	s.Router.HandleFunc("/fooditem", middlewares.SetMiddlewareJSON(s.CreateFoodItem)).Methods("POST")
 }
