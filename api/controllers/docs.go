84721ffaf95ac40513eef5419b04e74f9cbcbcb1
garcialuis <garcialuisdev@gmail.com>
2020-08-20 22:53:10 -0700

    + Documentation for foodItem controollers and routes using goswagger, + Serving swagger doc spec

---
+++ api/controllers/docs.go
@@ -0,0 +1,35 @@
+// Package classification Nutriport API
+//
+// Documentation for Nutriport API
+//
+//		Schemes: http
+//		BasePath: /
+//		Title: NutriportAPI
+//		Version: 1.0.0
+//
+//		Consumes:
+//		- application/json
+//
+//		Produces:
+//		- application/json
+//
+// swagger:meta
+package controllers
+
+import "github.com/garcialuis/Nutriport/api/models"
+
+// A single food item record
+// swagger:response foodItemResponse
+type foodItemResponseWrapper struct {
+	// Single food item
+	// in: body
+	Body models.FoodItem
+}
+
+// A list of food items
+// swagger:response foodItemsResponse
+type foodItemsResponseWrapper struct {
+	// All foodItems currently in db
+	// in: body
+	Body []models.FoodItem
+}
--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "github.com/garcialuis/Nutriport/api/middlewares"
+import (
+	"net/http"
+
+	"github.com/garcialuis/Nutriport/api/middlewares"
+	"github.com/go-openapi/runtime/middleware"
+)
 
 func (s *Server) InitializeRoutes() {
 	// Home
@@ -10,4 +15,11 @@ func (s *Server) InitializeRoutes() {
 	s.Router.HandleFunc("/fooditem/{foodName}", middlewares.SetMiddlewareJSON(s.GetFoodItemByName)).Methods("GET")
 	s.Router.HandleFunc("/fooditem", middlewares.SetMiddlewareJSON(s.CreateFoodItem)).Methods("POST")
 	s.Router.HandleFunc("/fooditem", middlewares.SetMiddlewareJSON(s.GetAllFoodItems)).Methods("GET")
+
+	// Swagger Docs:
+	opts := middleware.RedocOpts{SpecURL: "../../swagger.yaml"}
+	sh := middleware.Redoc(opts, nil)
+
+	s.Router.Handle("/docs", sh)
+	s.Router.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 }
