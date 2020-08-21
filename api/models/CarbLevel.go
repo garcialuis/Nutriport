2e67f87bf397ce5b16089e5c2bf284026369421b
garcialuis <garcialuisdev@gmail.com>
2020-08-20 22:12:03 -0700

    + documentation for models to be used later by swagger docs

--- api/models/CarbLevel.go
+++ api/models/CarbLevel.go
@@ -1,7 +1,15 @@
 package models
 
+// CarbLevel is a numeric rank representation of the carbohydrates present in the food item
+// The higher the level, the more carbohydrates the item has.
+//
+// swagger:model
 type CarbLevel struct {
+	// The id represents the carb level id: 1 = low, 2 = moderate, 3 = high in carbs
+	// required: true
+	// min: 1
 	ID uint8 `json:"id" gorm:"primary_key:auto_increment"`
-
+	// The description is a string describing the level, this will be either low, moderate, or high
+	// required: false
 	Description string `json:"description" gorm:"size:255;not null"`
 }
