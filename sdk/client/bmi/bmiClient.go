f4168db69fecb0900702c16f25b6fdc86a0baa14
garcialuis <garcialuisdev@gmail.com>
2020-09-10 09:38:09 -0700

    + BMI Client function to calculate imperial BMI, + Person model in SDK

---
+++ sdk/client/bmi/bmiClient.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/garcialuis/Nutriport/sdk/models"
+)
+
+type BMIClientService struct {
+}
+
+func NewBMIService() *BMIClientService {
+	return &BMIClientService{}
+}
+
+func (service *BMIClientService) CalculateImperialBMI(weight, height float64) models.Person {
+
+	personInfo := models.Person{}
+	client := &http.Client{}
+
+	weightStr := fmt.Sprintf("%f", weight)
+	heightStr := fmt.Sprintf("%f", height)
+
+	req, err := http.NewRequest("GET", "http://localhost:8085/imperial/bmi", nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	q := req.URL.Query()
+	q.Add("weight", weightStr)
+	q.Add("height", heightStr)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Println("Error sending request to server")
+		return models.Person{}
+	}
+
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(respBody, &personInfo)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return personInfo
+}
---
+++ sdk/models/Person.go
@@ -0,0 +1,11 @@
+package models
+
+type Person struct {
+	Weight         float64 `json:"weight,omitempty"`
+	Height         float64 `json:"height,omitempty"`
+	Gender         uint    `json:"gender,omitempty"`
+	Age            uint    `json:"age,omitempty"`
+	BMI            float64 `json:"BMI,omitempty"`
+	BMIDescription string  `json:"BMIDescription,omitempty"`
+	TEE            float64 `json:"TEE,omitempty"`
+}
