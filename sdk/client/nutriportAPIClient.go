cd560442a3c847a60c45b7b2ad54753b6ef8c86a
garcialuis <garcialuisdev@gmail.com>
2020-09-17 15:04:59 -0700

    * modified nutriportAPIClient.go in efforts to consolite all functionality clients into one.

--- sdk/client/nutriportAPIClient.go
+++ sdk/client/nutriportAPIClient.go
@@ -1,11 +1,28 @@
 package client
 
-import "github.com/garcialuis/Nutriport/sdk/client/fooditem"
+import (
+	"fmt"
+
+	"github.com/garcialuis/Nutriport/sdk/client/bmi"
+	"github.com/garcialuis/Nutriport/sdk/client/fooditem"
+)
 
 type NutriPortApis struct {
-	FoodClient fooditem.ClientService
+	FoodClient *fooditem.ClientService
+	BMIClient  *bmi.BMIClientService
+}
+
+func New() *NutriPortApis {
+
+	cli := new(NutriPortApis)
+	cli.FoodClient = fooditem.NewClientService()
+	cli.BMIClient = bmi.NewBMIService()
+
+	return cli
 }
 
 func NewTestFunc() {
-
+	npa := New()
+	info := npa.BMIClient.CalculateImperialBMI(12.0, 1234.5)
+	fmt.Println(info)
 }
