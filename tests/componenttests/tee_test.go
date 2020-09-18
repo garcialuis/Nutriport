b4ce39d4830a34a9f45f05e048e256a262f6cd14
garcialuis <garcialuisdev@gmail.com>
2020-09-17 18:16:08 -0700

    * modified tee_controller to route to 2 decimal places, + tee component tests: tee_test.go

---
+++ tests/componenttests/tee_test.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"testing"
+
+	nutriportclient "github.com/garcialuis/Nutriport/sdk/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculateTotalEnergyExpenditureMale(t *testing.T) {
+
+	nutriportClient := nutriportclient.New()
+	personInfo := nutriportClient.TEEClient.CalculateTotalEnergyExpenditure(25, 0, 143, "moderately active")
+
+	assert.Equal(t, 2006.83, personInfo.TEE)
+}
+
+func TestCalculateTotalEnergyExpenditureFemale(t *testing.T) {
+
+	nutriportClient := nutriportclient.New()
+	personInfo := nutriportClient.TEEClient.CalculateTotalEnergyExpenditure(25, 1, 130, "extremely active")
+
+	assert.Equal(t, 2888.40, personInfo.TEE)
+}
+
+func TestCalculateTotalEnergyExpenditureFemale2(t *testing.T) {
+
+	nutriportClient := nutriportclient.New()
+	personInfo := nutriportClient.TEEClient.CalculateTotalEnergyExpenditure(42, 1, 122.53, "sedentary")
+
+	assert.Equal(t, 568.50, personInfo.TEE)
+}
